16b0841926e80e488e24e26ba9c2c5c1039a702d
john-cheval <johncheval1@gmail.com>
2025-03-18 23:06:00 +0530

    over values section added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,12 @@
 import AboutHero from "@/components/About/AboutHero";
+import OurValues from "@/components/About/OurValues";
 import React from "react";
 
 const AboutPage = () => {
   return (
     <>
       <AboutHero />
+      <OurValues />
     </>
   );
 };
--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -21,3 +21,21 @@ export const aboutData = [
     imageUrl: "/About/image2.png",
   },
 ];
+
+export const aboutData2 = [
+  {
+    id: 1,
+    title: "Excellence in Service",
+    desc: "We are dedicated to providing our clients with an unmatched level of care, ensuring their visions are brought to life with precision and dedication.",
+  },
+  {
+    id: 2,
+    title: "Excellence in Craftsmanship",
+    desc: "We take pride in building the world’s most prestigious superyachts, setting new standards of quality and innovation.",
+  },
+  {
+    id: 3,
+    title: "Excellence in Team Growth",
+    desc: "We cultivate an environment where our team thrives, encouraging creativity, innovation, and continuous growth.",
+  },
+];
