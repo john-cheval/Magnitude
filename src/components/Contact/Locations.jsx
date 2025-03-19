571db528e8ed4311d8fb0c8a8851dad4f71e3208
john-cheval <johncheval1@gmail.com>
2025-03-19 15:03:28 +0530

    contact page has been done

--- src/components/Contact/Locations.jsx
+++ src/components/Contact/Locations.jsx
@@ -1,19 +1,44 @@
-import React from "react";
-import map from "../../../public/Contact/map.png";
-import Image from "next/image";
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const locations = [
+  { name: "Dubai", position: [25.276987, 55.296249] },
+  { name: "Hong Kong", position: [22.3193, 114.1694] },
+  { name: "Italy", position: [41.9028, 12.4964] },
+];
+
+// Custom Red Marker
+const redIcon = new L.Icon({
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x-red.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
 
 const Locations = () => {
   return (
     <section className="bg-altermain containers space-y-6">
       <h3 className="main-heading2">Our Locations</h3>
-      <Image
-        src={map}
-        alt="map"
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="w-full h-auto object-cover max-h-[478px]"
-      />
+      <MapContainer
+        center={[25, 55]}
+        zoom={2}
+        className="h-[478px] w-full rounded-lg"
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {locations.map((location, index) => (
+          <Marker key={index} position={location.position} icon={redIcon}>
+            <Popup>
+              <span className="text-white bg-red-600 px-2 py-1 rounded">
+                {location.name}
+              </span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </section>
   );
 };
