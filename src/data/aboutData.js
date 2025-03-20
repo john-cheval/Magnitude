67e7bf06523aeb54b2633224f7c792e87aaf733e
john-cheval <johncheval1@gmail.com>
2025-03-20 12:04:59 +0530

    about page api done

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,27 +1,3 @@
-export const aboutData = [
-  {
-    id: 1,
-    imageUrl: "/About/image1.png",
-  },
-  {
-    id: 2,
-    imageUrl: false,
-    title: "Magnitude",
-    desc: "Established in 2022 by a team of seasoned ship captains and project managers with decades of combined maritime expertise, Magnitude stands as a testament to knowledge gained through experience and an unwavering passion for excellence. Our mission is to deliver vessels that redefine innovation—combining cutting  edge design and technology with bespoke customization to meet the unique aspirations of each client. What truly sets us apart is our  profound understanding of the intricate complexities of superyacht construction. From the first sketch to the seamless, safe  operation of the yacht, we ensure a smooth and exceptional journey, bringing every detail of your vision to life.",
-  },
-
-  {
-    id: 3,
-    imageUrl: false,
-    title: "Our Mission",
-    desc: "At Magnitude, our mission is to craft extraordinary superyachts that surpass our clients' expectations while delivering a level of service that is truly unmatched. Every project is meticulously completed to specification, on schedule, and within budget. We take pride in making the construction journey effortless for our clients, expertly managing the complexities of shipyards, designers, and regulations so they can focus solely on realizing their vision.",
-  },
-  {
-    id: 4,
-    imageUrl: "/About/image2.png",
-  },
-];
-
 export const aboutData2 = [
   {
     id: 1,
