1ba25aa85efb34a4d9d15527cd5d4facf76bb4e6
john-cheval <johncheval1@gmail.com>
2025-03-24 12:52:49 +0530

    buyild error resolved

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,14 @@
 import React from "react";
 import generateMetadataData from "@/utils/generateMetaData";
 import { fetchData } from "@/utils/fetchData";
-import AboutPage from "@/pages/About";
+
+import dynamic from "next/dynamic";
+const AboutHero = dynamic(() => import("@/components/About/AboutHero"));
+
+const OurValues = dynamic(() => import("@/components/About/OurValues"));
+const WhyChooseUs = dynamic(() => import("@/components/About/WhyChooseUs"));
+// import OurValues from "@/components/About/OurValues";
+// import WhyChooseUs from "@/components/About/WhyChooseUs";
 
 export async function generateMetadata() {
   return await generateMetadataData(14, "about", false);
@@ -12,7 +19,18 @@ const About = async () => {
   );
   return (
     <>
-      <AboutPage aboutData={aboutData} />
+      <>
+        <AboutHero
+          banner={aboutData?.top_banner}
+          heading={aboutData?.small_heading}
+          list={aboutData?.section_list[0]?.list}
+        />
+        <OurValues ourValuesData={aboutData?.section_list[1]?.list} />
+        <WhyChooseUs
+          whyChooseData={aboutData?.section_list[2]?.list}
+          imageUrl={aboutData?.section_list[2]?.list[0]?.image}
+        />
+      </>
     </>
   );
 };
