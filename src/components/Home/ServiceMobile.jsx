38b02e041b8c850297128524183babd10a005b00
john-cheval <johncheval1@gmail.com>
2025-03-24 22:14:19 +0530

    animtion resposove done

--- src/components/Home/ServiceMobile.jsx
+++ src/components/Home/ServiceMobile.jsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
 const ServiceMobile = ({ title, serviceData }) => {
   const homeCardData = Object?.values(serviceData);
 
@@ -11,30 +10,28 @@ const ServiceMobile = ({ title, serviceData }) => {
         {title}
       </h3>
 
-      <div className="relative pt-8  h-fit w-screen overflow-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-2 l  relative ">
-          {homeCardData?.map((cardData, index) => (
-            <Link
-              href={`/services/${cardData?.post_name}`}
-              className="space-y-4 lg:space-y-6 cursor-pointer work-card"
-              prefetch={true}
-              key={cardData?.ID || index}
-            >
-              <Image
-                src={cardData?.home_page_image}
-                alt={cardData?.post_title}
-                className="w-full md:h-auto object-cover h-[350px]"
-                width={500}
-                height={300}
-                sizes="100vw"
-              />
-              <h5 className="text-altermain text-xl lg:text-2xl leading-[150%]">
-                {cardData?.post_title}
-              </h5>
-              <span className="seperator2Dark"></span>
-            </Link>
-          ))}
-        </div>
+      <div className="text-red-500 grid grid-cols-1 pt-8 space-y-8">
+        {homeCardData?.map((cardData, index) => (
+          <Link
+            key={index}
+            href={`/services/${cardData?.post_name}`}
+            prefetch={true}
+          >
+            <Image
+              src={cardData?.home_page_image}
+              alt={cardData?.post_title}
+              className="w-full h-auto object-cover h-[350px]--"
+              height={0}
+              width={0}
+              sizes="100vw"
+            />
+
+            <h5 className="text-altermain text-xl lg:text-2xl leading-[150%] mt-2">
+              {cardData?.post_title}
+            </h5>
+            <span className="seperator2Dark"></span>
+          </Link>
+        ))}
       </div>
     </div>
   );
