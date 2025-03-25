6529d144b4a8ede3e8c7004e2142cf4c797e696b
john-cheval <johncheval1@gmail.com>
2025-03-25 20:59:05 +0530

    seo3

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -87,8 +87,8 @@ const HomeHero = ({ title, link, linkText, videoUrl }) => {
           preload={isMobile ? "none" : "metadata"}
           width="100%"
           height="100%"
-          poster={"/Magnitude.jpg"}
-          className="w-screen h-[300px] md:h-full md:max-h-[288px] object-cover"
+          poster={"/Magnitude.png"}
+          className="w-screen h-[300px] md:h-full md:max-h-[288px]-- object-cover"
           src={videoSrc}
         />
       )}
